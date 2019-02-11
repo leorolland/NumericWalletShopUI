a8e0f296ddf02c17da5e94cebb2d6872d9b8b549
Léo <rolland.leo@orange.fr>
2019-02-11 19:43:27 +0100

    Ajout des composants de base

---
+++ src/infosclient.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="donneesClient.defined">
+    <h2>Données sur le client</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'infosclient',
+  data () {
+    return {
+       title: '',
+       donneesClient: {defined: false}
+    }
+  },
+}
+</script>
---
+++ src/main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <hr>
+    <button v-on:click="checkoutData">CheckoutData</button>
+    <hr>
+    <infosclient donneesClient=></infosclient>  
+  </div>
+</template>
+
+<script>
+import Infosclient from './infosClient';
+
+export default {
+  name: 'main',
+  data () {
+    return {
+       title: '',
+       donneesClient: {defined: false}
+    }
+  },
+  components: {
+    Infosclient
+  },
+  methods: {
+    checkoutData: function () {
+      this.donneesClient.defined = true
+    }
+  }
+}
+</script>
