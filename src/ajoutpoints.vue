3b9d440902aee4954677008105aa539f74ed8c08
Léo <rolland.leo@orange.fr>
2019-02-15 22:33:52 +0100

    Commencement classe ajoutPoints

---
+++ src/ajoutpoints.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="donneesClient.defined">
+    <form action="">
+      <input type="number">
+    </form>
+  </div>
+</template>
+
+<script>
+import Magasin from "./magasin";
+export default {
+  name: "ajoutpoints",
+  props: ["donneesClient"]
+};
+</script>
+
+<style>
+  div {
+    border: 1px dotted black;
+    padding: 8px;
+  }
+</style>
