7556c5a3243fc2028cdb735ac56eac662857171b
Léo <rolland.leo@orange.fr>
2019-03-12 23:09:06 +0100

    Envoi requete POST points

--- src/actionsclient.vue
+++ src/actionsclient.vue
@@ -2,20 +2,24 @@
   <div v-if="donneesClient.defined" class="container">
     <h3>Actions Client</h3>
     <div v-for="magasin in Object.entries(donneesClient.magasins)">
-      <div v-if="appParameters.type != 'magasin' || magasin[0] == appParameters.id" >
+      <div v-if="appParameters.type != 'magasin' || magasin[0] == appParameters.id">
         {{magasin[1].nom}}
         <ul>
           <li>
-            <button class="waves-effect waves-light btn">+ 1 point</button> <button class="waves-effect waves-light btn">- 1 point</button>
+            <button v-on:click="addPoints(magasin[0], 1)" class="waves-effect waves-light btn">+ 1 point</button>
+            <button v-on:click="addPoints(magasin[0], -1)" class="waves-effect waves-light btn">- 1 point</button>
           </li>
           <li>
-            <button class="waves-effect waves-light btn">+ 2 points</button> <button class="waves-effect waves-light btn">- 2 points</button>
+            <button v-on:click="addPoints(magasin[0], 2)" class="waves-effect waves-light btn">+ 2 points</button>
+            <button v-on:click="addPoints(magasin[0], -2)" class="waves-effect waves-light btn">- 2 points</button>
           </li>
           <li>
-            <button class="waves-effect waves-light btn">+ 5 points</button> <button class="waves-effect waves-light btn">- 5 points</button>
+            <button v-on:click="addPoints(magasin[0], 5)" class="waves-effect waves-light btn">+ 5 points</button>
+            <button v-on:click="addPoints(magasin[0], -5)" class="waves-effect waves-light btn">- 5 points</button>
           </li>
-          <li>
-            Ajouter <input type="number" class="field" value="0"> points <button class="waves-effect waves-light btn">Valider</button>
+          <li>Ajouter
+            <input type="number" class="field" value="0"> points
+            <button class="waves-effect waves-light btn">Valider</button>
           </li>
         </ul>
       </div>
@@ -27,14 +31,31 @@
 import Magasin from "./magasin";
 export default {
   name: "actionsclient",
-  props: ["donneesClient", "appParameters"]
+  props: ["donneesClient", "appParameters"],
+  methods: {
+    addPoints: function(magasinId, points) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://noxunote.fr:3000/ajoutePoints");
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.send(JSON.stringify({
+        "idc": this.donneesClient.clientId,
+        "idm": magasinId,
+        "points:": points
+      }));
+      // Quand la réponse de l'API est reçue
+      xhr.onreadystatechange = e => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          this.$toaster.success(points + ' points ont été ajoutés à ce compte.')
+        }
+      };
+    }
+  }
 };
 </script>
 
 <style>
-
-  li {
-    line-height: 2em;
-    text-emphasis: none;
-  }
+li {
+  line-height: 2em;
+  text-emphasis: none;
+}
 </style>
